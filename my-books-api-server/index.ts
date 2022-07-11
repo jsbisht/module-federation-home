ec51a454768fb084a64aea4dec183e520b9ce31f
jsbisht <jagdeepbisht89@gmail.com>
2022-07-11 20:37:09 +0530

    updated api server for proxy and added nodemon for api servers

--- my-books-api-server/index.ts
+++ my-books-api-server/index.ts
@@ -3,25 +3,29 @@ import httpProxy from 'http-proxy'
 
 const app = express()
 const port = 8888
-const apiProxy = httpProxy.createProxyServer()
+const proxy = httpProxy.createProxyServer()
 const homeApi = 'http://localhost:9001'
 const upsellApi = 'http://localhost:9002'
 const profileApi = 'http://localhost:9003'
 
+proxy.on('error', function (e) {
+  console.error(e)
+})
+
 /* Setting up reverse proxy */
-app.all('/home/*', function (req, res) {
+app.use('/home', function (req, res) {
+  proxy.web(req, res, { target: homeApi, forward: homeApi })
   console.log('redirecting to Server1')
-  apiProxy.web(req, res, { target: homeApi })
 })
 
-app.all('/upsell/*', function (req, res) {
+app.use('/upsell', function (req, res) {
   console.log('redirecting to Server2')
-  apiProxy.web(req, res, { target: upsellApi })
+  proxy.web(req, res, { target: upsellApi })
 })
 
-app.all('/profile/*', function (req, res) {
+app.use('/profile', function (req, res) {
   console.log('redirecting to Server3')
-  apiProxy.web(req, res, { target: profileApi })
+  proxy.web(req, res, { target: profileApi })
 })
 
 /* listening of port in reverse proxy server */
